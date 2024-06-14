6515ff59c03855b20fa48ae6d2c9e3a0dbbd4205
jvnnifer <janejennifer56@gmail.com>
2024-06-14 10:14:41 +0700

    udah semua kecuali gambar

--- main-project/src/pages/treasure/VoiceOver.jsx
+++ main-project/src/pages/treasure/VoiceOver.jsx
@@ -1,29 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import photo2 from "/logo2.svg";
 import photo4 from "/logo1.svg";
 import photo1 from "/male.svg";
 import photo3 from "/female.svg";
 
-const VoiceOver = () => {
+const VoiceOver = ({ category }) => {
+  const [portfolios, setPortfolios] = useState([]);
+
+  useEffect(() => {
+    const fetchPortfolios = async () => {
+      try {
+        console.log("Fetching portfolios for category:", category);
+        const response = await axios.get(`http://localhost:8081/illustration/${category}`, {
+          params: { category },
+        });
+        setPortfolios(response.data);
+      } catch (error) {
+        console.error("There was an error fetching the data!", error);
+      }
+    };
+    fetchPortfolios();
+  }, [category]);
   return (
     <div>
       <div class="flex flex-col lg:flex-row m-5">
-        <div className="flex flex-col w-64 mx-20">
-          <img src={photo1} className="w-64" />
-          <div className="flex flex-row items-center pt-3">
-            <img src={photo2} />
-            <h1 className="font-semibold pl-2">Van Houtten</h1>
+        {portfolios.map((portfolio) => (
+          <div key={portfolio.NoJasa} className="flex flex-col w-64 mx-20">
+            <img src={photo1} className="w-64" alt="" />
+            <div className="flex flex-row items-center pt-3">
+              <img src={photo2} alt="" />
+              <a href="#" className="font-semibold pl-2">
+                {portfolio.username}
+              </a>
+            </div>
+            <p className="font-semibold">{portfolio.NamaJenisJasa}</p>
           </div>
-          <p className="font-semibold">I will do a modern minimalist logo design for your business</p>
-        </div>
-        <div className="flex flex-col w-64 mx-20">
-          <img src={photo3} className="w-64" />
-          <div className="flex flex-row items-center pt-3">
-            <img src={photo4} />
-            <h1 className="font-semibold pl-2">Mangrove</h1>
-          </div>
-          <p className="font-semibold">I will design, edit, redo, resize your logo</p>
-        </div>
+        ))}
       </div>
     </div>
   );
